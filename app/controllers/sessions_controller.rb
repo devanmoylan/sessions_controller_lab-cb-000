067e3d099afdcd7270e7e2bac0aaa33383f806c4
Devan <devan.moylan@backyardlion.com>
2017-07-31 20:05:01 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
   end
 
   def destroy
+    if session[:name].nil?
+      redirect_to root_path
+    else
+      session.clear
+      redirect_to root_path
+    end
   end
+
 end
